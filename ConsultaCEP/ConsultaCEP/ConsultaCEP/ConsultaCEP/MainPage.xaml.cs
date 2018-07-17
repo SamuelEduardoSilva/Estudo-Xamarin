633e0731ac61a5f5c81174643da2e78ee598e7cb
Samuel Eduardo <samueleduardo1609@hotmail.com>
2018-07-17 18:33:25 -0300

    att

--- ConsultaCEP/ConsultaCEP/ConsultaCEP/ConsultaCEP/MainPage.xaml.cs
+++ ConsultaCEP/ConsultaCEP/ConsultaCEP/ConsultaCEP/MainPage.xaml.cs
@@ -6,32 +6,66 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using ConsultaCEP.Servico;
 using ConsultaCEP.Servico.Modelo;
+using System.ComponentModel;
+
 namespace ConsultaCEP
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
+	public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        private bool isLoading;
+        public bool IsLoading
+        {
+            get
+            {
+                return this.isLoading;
+            }
+            set
+            {
+                this.isLoading = value;
+                RaisePropertyChanged("IsLoading");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        public MainPage()
 		{
 			InitializeComponent();
-
+            
+            loadingOverlay.IsVisible = false;
+            BindingContext = this;
             BOTAO.Clicked += BuscarCEP;
 		}
 
-        private void BuscarCEP(object sender, EventArgs args)
+        private async void BuscarCEP(object sender, EventArgs args)
         {
-        
+            loadingOverlay.IsVisible = true;
             // TODO - Validações
-            string cep = CEP.Text.Trim();
+
+            string cep=  CEP.Text.Trim();
 
             if(isValidCEP(cep))
             {
                 try
                 {
+                    
+                    loadingOverlay.IsVisible = true;
+                    await Task.Delay(2000);
                     Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
-                    if(end != null)
+                    await Task.Delay(1000);
+                    loadingOverlay.IsVisible = false;
+                    if (end != null)
                     {
                         Console.WriteLine(end.localidade, end.uf, end.logradouro, end.bairro);
                         System.Diagnostics.Debug.WriteLine(end.localidade, end.uf, end.logradouro, end.bairro);
+                        
+                        
                         RESULTADO.Text = string.Format("Endereço: {2} {3} {0} {1}", end.localidade, end.uf, end.logradouro, end.bairro);
                     }
                     else
@@ -40,6 +74,7 @@ namespace ConsultaCEP
                     }
                 }catch(Exception e)
                 {
+                    loadingOverlay.IsVisible = false;
                     DisplayAlert("ERRO CRÍTICO", e.Message, "OK");
                 }
                 
@@ -66,5 +101,19 @@ namespace ConsultaCEP
 
             return true;
         }
-	}
+       
+
+        public async void aparece()
+        {
+            IsLoading = true;
+        }
+
+        public async void some()
+        {
+           IsLoading = false;
+        }
+  
+        
+    }
 }
+
